396706a3c99c39152a757a18cfdc67fa7e9830b3
Benji Decker <bensdecker@gmail.com>
2013-03-26 17:55:10 -0400

    got numbers reordered and clear function implemented

--- calculator.rb
+++ calculator.rb
@@ -2,18 +2,48 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
   stack :margin => 20 do
     @output = edit_line
     
-    flow do
-      %w(0 1 2 3 4 5 6 7 8 9 + / * -).each do |op|      
-        button op do         
-          append op
+    stack do
+      
+      flow do
+        %w(7 8 9 +).each do |op|      
+          button op do         
+            append op
+          end
         end
       end
-      
-      button "=" do
-        eval_expression
+
+      flow do
+        %w(4 5 6 -).each do |op|      
+          button op do         
+            append op
+          end
+        end
       end
-    end
+
+      flow do
+        %w(1 2 3 *).each do |op|      
+          button op do         
+            append op
+          end
+        end
+      end
+
+      flow do
+        button "0" do         
+          append "0"
+        end
+        button "=" do
+          eval_expression
+        end
+        button "C" do
+          clear_expression
+        end
+        button "/" do         
+          append "/"
+        end      
+      end
     
+    end
   end
   
   # Stick a string on the end of our input
@@ -32,5 +62,11 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
     @input = eval(@input).to_s
     @output.text = @input
   end
-  
+
+  # Clear the input we've got so far
+  def clear_expression
+    @input = ""
+    @output.text = ""
+  end
+
 end
